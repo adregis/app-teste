734fe36b6cc8a57c6de41a141a0292d83662e79b
adriano <adregis2000@yahoo.com>
2021-11-08 22:02:41 -0300

    Add config docker

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    return flask.Response('Teste OK!')
+    return flask.Response('Site - Teste OK!')
 
 if __name__ == '__main__':
     app.run() 
